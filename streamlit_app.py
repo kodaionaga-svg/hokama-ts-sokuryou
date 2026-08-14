01abdbdd642eeb1267d0ab647841ebc5dba6965b
kodaionaga-svg <kodaionaga@gmail.com>
2026-08-14 17:23:41 +0900

    測量計算画面を表示

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
-import streamlit as st
+from pathlib import Path
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import streamlit.components.v1 as components
+
+
+st_html = Path(__file__).with_name("index.html").read_text(encoding="utf-8")
+components.html(st_html, height=1300, scrolling=True)
